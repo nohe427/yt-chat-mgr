adb1c09e7c0fb4a7b1706fa4a1e4ddd9ca5a5ea3
Alexander Nohe <alex.nohe427@gmail.com>
2024-02-03 19:58:20 -0800

    Add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration()]
+  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"yt-chat-section","appId":"1:275285882972:web:d3e8b06bfa87526c084e99","storageBucket":"yt-chat-section.appspot.com","apiKey":"AIzaSyBAl4IrLR5n8lyQt44dphWVliiadLBAZGo","authDomain":"yt-chat-section.firebaseapp.com","messagingSenderId":"275285882972"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
